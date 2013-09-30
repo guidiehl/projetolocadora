1c9bb92166c2917ce77c2a9a41dc090e15505283
guest01 <guest01@10.1.49.53>
2013-09-30 09:37:45 -0300

    add classe cliente

---
+++ src/Cliente.java
@@ -0,0 +1,21 @@
+
+public class Cliente extends Pessoa {
+    
+    private double saldoDisponivel;
+
+    public Cliente() {
+    }
+
+    public double getSaldoDisponivel() {
+        return saldoDisponivel;
+    }
+
+    public void setSaldoDisponivel(double saldoDisponivel) {
+        this.saldoDisponivel = saldoDisponivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "saldoDisponivel=" + saldoDisponivel + '}';
+    }
+}
